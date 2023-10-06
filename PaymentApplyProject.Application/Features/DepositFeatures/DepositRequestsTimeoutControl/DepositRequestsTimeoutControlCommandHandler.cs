b88a769e2766d0776c983ef22e05f1fb55e3f94a
Akif AKSU <akif.aksu@drd.com.tr>
2023-10-06 20:04:36 +0300

    düzenlme

--- PaymentApplyProject.Application/Features/DepositFeatures/DepositRequestsTimeoutControl/DepositRequestsTimeoutControlCommandHandler.cs
+++ PaymentApplyProject.Application/Features/DepositFeatures/DepositRequestsTimeoutControl/DepositRequestsTimeoutControlCommandHandler.cs
@@ -35,7 +35,7 @@ namespace PaymentApplyProject.Application.Features.DepositFeatures.DepositReques
         {
             var timeoutDepositRequests = _paymentContext.DepositRequests.Where(x =>
             (x.Deposit == null || x.Deposit.Deleted)
-            && (x.ValidTo.HasValue ? x.ValidTo < DateTimeExtensions.LocalNow : x.AddDate < DateTimeExtensions.LocalNow.AddMinutes(-10))
+            && (x.ValidTo.HasValue ? x.ValidTo < DateTimeExtensions.LocalNow : x.AddDate < DateTimeExtensions.LocalNow.AddHours(-1))
             && !x.Deleted).ToList();
 
             if (!timeoutDepositRequests.Any())
