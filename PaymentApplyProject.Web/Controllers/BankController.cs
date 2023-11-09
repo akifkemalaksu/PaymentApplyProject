88221e28ae3e7c0d9d9bc4e60cdb2273e9c1e5de
Akif AKSU <akif.aksu@drd.com.tr>
2023-11-09 10:33:01 +0300

    banka yetkilendirme

--- PaymentApplyProject.Web/Controllers/BankController.cs
+++ PaymentApplyProject.Web/Controllers/BankController.cs
@@ -12,72 +12,77 @@ using PaymentApplyProject.Application.Features.BankFeatures.LoadBanksForDatatabl
 
 namespace PaymentApplyProject.Web.Controllers
 {
-    [Authorize(Roles = "admin,user")]
-    public class BankController : CustomController
-    {
-        private readonly IMediator _mediator;
+	[Authorize(Roles = "admin,user")]
+	public class BankController : CustomController
+	{
+		private readonly IMediator _mediator;
 
-        public BankController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+		public BankController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult BankAccounts()
-        {
-            return View();
-        }
+		public IActionResult BankAccounts()
+		{
+			return View();
+		}
 
-        public IActionResult ViewAddBankAccountPartial()
-        {
-            return PartialView("_viewAddBankAccountPartial");
-        }
+		[Authorize(Roles = "admin")]
+		public IActionResult ViewAddBankAccountPartial()
+		{
+			return PartialView("_viewAddBankAccountPartial");
+		}
 
-        [Route("[controller]/[action]/{id}")]
-        public async Task<IActionResult> ViewEditBankAccountPartial(int id)
-        {
-            var result = await _mediator.Send(new GetBankAccountByIdQuery { Id = id });
-            return PartialView("_viewEditBankAccountPartial", result);
-        }
+		[Authorize(Roles = "admin")]
+		[Route("[controller]/[action]/{id}")]
+		public async Task<IActionResult> ViewEditBankAccountPartial(int id)
+		{
+			var result = await _mediator.Send(new GetBankAccountByIdQuery { Id = id });
+			return PartialView("_viewEditBankAccountPartial", result);
+		}
 
 
-        [HttpPost]
-        public async Task<IActionResult> LoadBanks(LoadBanksForDatatableQuery loadBanksForDatatableQuery)
-        {
-            var result = await _mediator.Send(loadBanksForDatatableQuery);
-            return Json(result);
-        }
+		[HttpPost]
+		public async Task<IActionResult> LoadBanks(LoadBanksForDatatableQuery loadBanksForDatatableQuery)
+		{
+			var result = await _mediator.Send(loadBanksForDatatableQuery);
+			return Json(result);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> LoadBankAccounts(LoadBankAccountsForDatatableQuery loadBankAccountsForDatatableQuery)
-        {
-            var result = await _mediator.Send(loadBankAccountsForDatatableQuery);
-            return Json(result);
-        }
+		[HttpPost]
+		public async Task<IActionResult> LoadBankAccounts(LoadBankAccountsForDatatableQuery loadBankAccountsForDatatableQuery)
+		{
+			var result = await _mediator.Send(loadBankAccountsForDatatableQuery);
+			return Json(result);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> AddBankAccount(AddBankAccountCommand addBankAccountCommand)
-        {
-            var result = await _mediator.Send(addBankAccountCommand);
-            return CreateResult(result);
-        }
+		[Authorize(Roles = "admin")]
+		[HttpPost]
+		public async Task<IActionResult> AddBankAccount(AddBankAccountCommand addBankAccountCommand)
+		{
+			var result = await _mediator.Send(addBankAccountCommand);
+			return CreateResult(result);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> EditBankAccount(EditBankAccountCommand editBankAccountCommand)
-        {
-            var result = await _mediator.Send(editBankAccountCommand);
-            return CreateResult(result);
-        }
+		[Authorize(Roles = "admin")]
+		[HttpPost]
+		public async Task<IActionResult> EditBankAccount(EditBankAccountCommand editBankAccountCommand)
+		{
+			var result = await _mediator.Send(editBankAccountCommand);
+			return CreateResult(result);
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> DeleteBankAccount([FromBody] DeleteBankAccountCommand deleteBankAccountCommand)
-        {
-            var result = await _mediator.Send(deleteBankAccountCommand);
-            return CreateResult(result);
-        }
-    }
+		[Authorize(Roles = "admin")]
+		[HttpPost]
+		public async Task<IActionResult> DeleteBankAccount([FromBody] DeleteBankAccountCommand deleteBankAccountCommand)
+		{
+			var result = await _mediator.Send(deleteBankAccountCommand);
+			return CreateResult(result);
+		}
+	}
 }
